5a70cdf898d5d71a3ab9e09f35865681506692c5
rjunio98 <robledorjunior1@gmail.com>
2024-02-06 02:57:28 -0300

    Adiciona componentes SkeltonLoading, BusinessList e BusinessItem

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 "use client";
 
 import GlobalApi from "@/Shared/GlobalApi";
+import BusinessList from "@/components/Home/BusinessList";
 import CategoryList from "@/components/Home/CategoryList";
 import GoogleMapView from "@/components/Home/GoogleMapView";
 import RangeSelect from "@/components/Home/RangeSelect";
 import SelectRating from "@/components/Home/SelectRating";
+import SkeltonLoading from "@/components/SkeltonLoading";
 import { UserLocationContext } from "@/context/UserLocationContext";
 import { signOut, useSession } from "next-auth/react";
 import Image from "next/image";
@@ -16,6 +18,8 @@ export default function Home() {
   const [category, setCategory] = useState();
   const [radius, setRadius] = useState(2500);
   const [businessList, setBusinessList] = useState([]);
+  const [businessListOrg, setBusinessListOrg] = useState([]);
+  const [loading, setLoading] = useState(false);
   const router = useRouter();
   const { userLocation, setUserLocation } = useContext(UserLocationContext);
 
@@ -30,6 +34,10 @@ export default function Home() {
   }, [category, radius]);
 
   const getGooglePlace = () => {
+    if (category) {
+      setLoading(true);
+    }
+
     GlobalApi.getGooglePlace(
       category,
       radius,
@@ -37,18 +45,54 @@ export default function Home() {
       userLocation.lng
     ).then((resp) => {
       setBusinessList(resp.data.product.results);
+      setBusinessListOrg(resp.data.product.results);
+      setLoading(false);
     });
   };
 
+  const onRatingChange = (rating) => {
+    if (rating.length == 0) {
+      setBusinessList(businessListOrg);
+    }
+    const result = businessList.filter((item) => {
+      for (let i = 0; i < rating.length; i++) {
+        if (item.rating >= rating[i]) {
+          return true;
+        }
+        return false;
+      }
+    });
+
+    console.log(result);
+  };
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 h-screen">
+    <div
+      className="grid 
+    grid-cols-1
+    md:grid-cols-4 "
+    >
       <div className="p-3">
         <CategoryList onCategoryChange={(value) => setCategory(value)} />
         <RangeSelect onRadiusChange={(value) => setRadius(value)} />
-        <SelectRating />
+        <SelectRating onRatingChange={(value) => onRatingChange(value)} />
       </div>
       <div className=" col-span-3">
-        <GoogleMapView />
+        <GoogleMapView businessList={businessList} />
+        <div
+          className="md:absolute mx-2 w-[90%] md:w-[74%]
+           bottom-36 relative md:bottom-3"
+        >
+          {!loading ? (
+            <BusinessList businessList={businessList} />
+          ) : (
+            <div className="flex gap-3">
+              {[1, 2, 3, 4, 5].map((item, index) => (
+                <SkeltonLoading key={index} />
+              ))}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
